c677d99473b0faef043fa479cb3b5e4394fbcc52
mmehul2403 <mmehul2403@gmail.com>
2024-07-11 19:20:04 -0400

    BUg fix: https://conestogac-team-nkwc0uaj.atlassian.net/browse/KAN-24 1

--- index.js
+++ index.js
@@ -75,11 +75,22 @@ async function startServer() {
     console.log("Database connection has been established successfully.");
     // started Apollo Server
     await server.start();
-    //Configured apollo
+    //Configured apollo and react application
+    const allowedOrigins = [
+      "https://studio.apollographql.com",
+      "http://localhost:3000",
+    ];
     const corsOptions = {
-      origin: "https://studio.apollographql.com",
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       credentials: true,
     };
+
     app.use(cors(corsOptions));
 
     // Added the Apollo GraphQL middleware and set the path to /graphql or /api
