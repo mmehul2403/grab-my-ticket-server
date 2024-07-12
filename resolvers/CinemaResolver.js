53ec9dc85c3de80ce27c48f0cff07c2fad6f2eb2
Emma Wang <lemeiy.wang@gmail.com>
2024-07-12 15:32:31 -0400

    cinema query:add result without filter

--- resolvers/CinemaResolver.js
+++ resolvers/CinemaResolver.js
@@ -1,23 +1,14 @@
 const { Op } = require("sequelize");
 const sequelizeDatabase = require("../config/database");
 const Cinema = require("../models/Cinema")(sequelizeDatabase);
-/*
-type Query {
-  queryCinemaBy(cinema_name: String!): [Cinema]
-  queryCinemaById(cinema_id: Int!): Cinema
-}
 
-type Mutation {
-  createCinema(cinema: CinemaInput!): Result
-  updateCinema(cinema: CinemaInput!): Result
-  deleteCinema(cinema_id: Int!): Result
-  
-}
-*/
 const CinemaResolver = {
   Query: {
     queryCinemaBy: async (_, args) => {
       let cinema_name = args.cinema_name;
+      if (!cinema_name) {
+        return await Cinema.findAll();
+      }
       return await Cinema.findAll({
         where: {
           cinema_name: {
