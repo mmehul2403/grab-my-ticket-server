4f366fd9d696a8368e4e0c0f10a7eabdfeb86b31
mmehul2403 <mmehul2403@gmail.com>
2024-07-19 17:51:51 -0400

    reform express server setup

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ApolloServer } = require("apollo-server-express");
-const { graphqlUploadExpress, GraphQLUpload } = require("graphql-upload");
+const { graphqlUploadExpress } = require("graphql-upload");
 const sequelizeDatabase = require("./config/database");
-
 const dbconfig = require("./config/db_config.json")[
   process.env.NODE_ENV || "development"
 ];
-
 const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
 const path = require("path");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const session = require("express-session");
-var MongoDBStore = require("connect-mongodb-session")(session);
+const MongoDBStore = require("connect-mongodb-session")(session);
 const cors = require("cors");
 
 // Sync the Movie model with the database
@@ -23,10 +21,8 @@ sequelizeDatabase.sync({ force: false }).then(() => {
 async function startServer() {
   const app = express();
   app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-  app.use(graphqlUploadExpress());
   app.use(bodyParser.json());
 
-  console.log("MongoDB Connection is :" + dbconfig.mongoUrl);
   const session_store = new MongoDBStore(
     {
       uri: dbconfig.mongoUrl,
@@ -42,6 +38,23 @@ async function startServer() {
     console.log("###" + error);
   });
 
+  const allowedOrigins = [
+    "https://studio.apollographql.com",
+    "http://localhost:3000",
+  ];
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
+
   app.use(
     session({
       secret: "Grab my ticket session.",
@@ -49,19 +62,23 @@ async function startServer() {
       saveUninitialized: true,
       store: session_store,
       cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+        secure: false, // set to true if using HTTPS
       },
     })
   );
+
+  app.use(graphqlUploadExpress());
+
   const typesArray = loadFilesSync(path.join(__dirname, "./schema"));
-  let mergeTypeDefsRes = mergeTypeDefs(typesArray);
+  const mergeTypeDefsRes = mergeTypeDefs(typesArray);
   const resolversArr = loadFilesSync(path.join(__dirname, "./resolvers"));
-  let mergeResolversRes = mergeResolvers(resolversArr);
+  const mergeResolversRes = mergeResolvers(resolversArr);
 
   const server = new ApolloServer({
     typeDefs: mergeTypeDefsRes,
     resolvers: mergeResolversRes,
-    uploads: false, //I disabled built in upload handeling of GraphQL
+    uploads: false, // Disable built-in upload handling
     context: async ({ req, res }) => ({
       req,
       res,
@@ -72,27 +89,7 @@ async function startServer() {
   try {
     await sequelizeDatabase.authenticate();
     console.log("Database connection has been established successfully.");
-    // started Apollo Server
     await server.start();
-    //Configured apollo and react application
-    const allowedOrigins = [
-      "https://studio.apollographql.com",
-      "http://localhost:3000",
-    ];
-    const corsOptions = {
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      credentials: true,
-    };
-
-    app.use(cors(corsOptions));
-
-    // Added the Apollo GraphQL middleware and set the path to /graphql or /api
     server.applyMiddleware({ app, path: "/graphql", cors: false });
 
     app.listen({ port: 4000 }, () => {
